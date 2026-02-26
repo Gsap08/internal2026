d596464d8be9022acbdaeaabf2809d16d55a3210
Gsap08 <20140@rosmini.school.nz>
2026-02-26 17:58:29 +1300

    app/page routing
    started flask

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
+app = Flask(__name__)
+
+@app.route("/")
+def signinpage():
+    return render_template("SignInPage.html")
+
+@app.route("/home")
+def HomePage():
+    return render_template("HomePage.html")
+
+@app.route("/resources")
+def Resources():
+    return render_template("Resources.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
