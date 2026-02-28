26bd4ef9407d79cd5babd4e858d2d0c15e0a9cea
Gsap08 <20140@rosmini.school.nz>
2026-02-28 23:31:43 +1300

    sign up + reg |

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,9 +14,51 @@ def HomePage():
 def Resources():
     return render_template("Resources.html")
 
-@app.route("/register")
+@app.route("/registration", methods=['GET','POST'])
 def Register():
-    return render_template("Register.html")
+    if request.method == 'POST':
+        full_name = request.form['full_name']
+        email = request.form['email']
+        year_level = request.form['year_level']
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('db\\akoconnect.db')  
+        cursor = conn.cursor()
+        # cursor.execute('SELECT * FROM "User"')
+        # existing_user = cursor.fetchone()
+        # if existing_user:
+        #     conn.close()
+        ##Inserting a new user
+        cursor.execute("INSERT INTO User (full_name, email, year_level, username, password) VALUES (?, ?, ?, ?, ?)", (full_name, password, year_level, username, password ))
+        conn.commit()
+        conn.close()
+        return redirect('{{url_for("Login")}}')
+    else:
+        return render_template("Registration.html")
+        
+    
+
+
+
+@app.route('/login', methods=['POST']) ##login
+def Login():
+    username = request.form['username']
+    password = request.form['password']
+
+    conn = sqlite3.connect('akoconnect.db')  
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+    user = cursor.fetchone()
+
+
+    conn.close()
+
+    if user:
+        return render_template ("HomePage.html")
+    else:
+        return "Invalid Username or Password."
 
 if __name__ == "__main__":
     app.run(debug=True)
